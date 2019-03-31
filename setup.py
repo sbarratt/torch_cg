3059ce8498e0bd4bd401e57ef12fd377553c1982
Shane Barratt <sbarratt@berkeley.edu>
2019-03-31 11:27:28 -0700

    changed setup.py

--- setup.py
+++ setup.py
@@ -1,34 +1,6 @@
 import setuptools
 from torch.utils.cpp_extension import BuildExtension, CppExtension
 
-setuptools.setup(
-    name="cg_cpp",
-    ext_modules=[
-        CppExtension('cg_cpp', ['cpp/cg.cpp']),
-    ],
-    cmdclass={
-        'build_ext': BuildExtension
-    }
-)
-
-setuptools.setup(
-    name="torch_cg",
-    version="0.0.1",
-    author="Shane Barratt",
-    author_email="stbarratt@gmail.com",
-    description="pytorch conjugate gradient",
-    url="https://github.com/sbarratt/torch_cg",
-    license="MIT",
-    platforms="any",
-    packages=["torch_cg"],
-    classifiers=[
-        "Programming Language :: Python :: 3",
-        "License :: OSI Approved :: MIT License",
-        "Operating System :: OS Independent",
-    ],
-    install_requires=[
-        'torch',
-        'numpy',
-        'scipy'
-    ]
-)
+setuptools.setup(name='torch_cg',
+      ext_modules=[CppExtension('cg', ['cpp/cg.cpp'])],
+      cmdclass={'build_ext': BuildExtension})
